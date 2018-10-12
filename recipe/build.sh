69de0d174ded15177cbdf9eb5d65f21f5c32b440
Christian Brueffer <christian@brueffer.io>
2018-10-12 10:24:42 +0200

    Recipe cleanup.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-export PROJ_LIB=${PREFIX}/share/proj
-$R CMD INSTALL --build .
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  $R CMD INSTALL --build .
+else
+  mkdir -p $PREFIX/lib/R/library/sf
+  mv * $PREFIX/lib/R/library/sf
+fi
