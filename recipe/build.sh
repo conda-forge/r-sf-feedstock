a977bee7412eb756b61462045f823a0b893ef7f8
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-14 22:17:04 -0300

    fix typo and remove uncecessray env var

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,10 @@
 # This is just to get around a configure failure when trying to link to gdal.
 if [[ ${target_platform} == osx-64 ]]; then
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
-  export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,§{PREFIX}/lib"
+  export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 elif [[ ${target_platform} =~ .*linux.* ]]; then
   # This is to keep configure happy when it compiles a test proj4 project. Urgh.
   export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
-export PROJ_LIB=${PREFIX}/share/proj
+
 $R CMD INSTALL --build .
