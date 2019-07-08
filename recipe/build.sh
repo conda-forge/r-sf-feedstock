06293b8b0f3e1f6f15d816dca954c8dd728c0f23
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-07-08 10:15:15 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 export PKG_CPPFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1"
 export PKG_LIBS="-L$PREFIX/lib"
+export PROJ_LIB=${PREFIX}/share/proj
 # This is just to get around a configure failure when trying to link to gdal.
 if [[ ${target_platform} == osx-64 ]]; then
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
