048e97e9db919de157e98fb8f7f1834592a5906f
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-07-08 10:00:32 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ export PKG_CPPFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1
 export PKG_LIBS="-L$PREFIX/lib"
 # This is just to get around a configure failure when trying to link to gdal.
 if [[ ${target_platform} == osx-64 ]]; then
-  export CXX="${CXX} --std=c++17 -Wl,-rpath,${PREFIX}/lib"
+  export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
 fi
 
 $R CMD INSTALL --build .
