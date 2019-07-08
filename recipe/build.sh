8680b0b6b286d6dca37a12fe591a0a8157ac3449
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-07-08 09:47:03 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 
 # This is just to get around a configure failure when trying to link to gdal.
 if [[ ${target_platform} == osx-64 ]]; then
-  export CXX="${CXX} --std=c++11 -Wl,-rpath,${PREFIX}/lib"
+  export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
 fi
 
 $R CMD INSTALL --build .
