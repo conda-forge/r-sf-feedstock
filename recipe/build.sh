4949f0c04f92e6fd83f527a4e6e5b9394171b0d8
Devon Ryan <dpryan79@users.noreply.github.com>
2020-04-07 00:47:06 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -o errexit -o pipefail
+
+export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
+# This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
+if [[ ${target_platform} == osx-64 ]]; then	else
+  export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
+fi
+
 if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   ${R} CMD INSTALL --build .
