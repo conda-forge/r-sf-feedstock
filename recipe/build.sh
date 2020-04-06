9f682afde967028d069e7ef15d8056ef39949904
Devon Ryan <dpryan79@users.noreply.github.com>
2020-04-07 00:55:01 +0200

    silly copy-paste error

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ set -o errexit -o pipefail
 
 export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 # This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
-if [[ ${target_platform} == osx-64 ]]; then	else
+if [[ ${target_platform} == osx-64 ]]; then
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
 fi
 
