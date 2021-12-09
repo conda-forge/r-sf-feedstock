97abc67bc541004fa0884c606aa36ce9fd9b5b9e
akrherz <akrherz@iastate.edu>
2021-12-08 22:09:32 -0600

    try -DHAVE_WORKING_LOG1P workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
+# suggested workaround to conda-forge/r-base-feedstock#163
+export CXXFLAGS="-DHAVE_WORKING_LOG1P"
+export CPPFLAGS="${CPPFLAGS} -DHAVE_WORKING_LOG1P -Wl,-rpath,${PREFIX}/lib"
 # This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
 if [[ ${target_platform} == osx-64 ]]; then
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
