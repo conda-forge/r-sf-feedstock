bc596b6efda54ec38b789752b0aeb06f19e76376
akrherz <akrherz@iastate.edu>
2021-12-08 22:33:22 -0600

    set PKG_CPPFLAGS this time

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-# suggested workaround to conda-forge/r-base-feedstock#163
-export CXXFLAGS="-DHAVE_WORKING_LOG1P"
-export CPPFLAGS="${CPPFLAGS} -DHAVE_WORKING_LOG1P -Wl,-rpath,${PREFIX}/lib"
+# workarond conda-forge/r-base-feedstock#163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+# NB: unsure if this is even used...
+export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 # This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
 if [[ ${target_platform} == osx-64 ]]; then
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
