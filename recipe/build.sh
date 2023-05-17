81d4737275d56abb33539ee84bb003fd5765e69c
Mervin Fansler <mef3005@med.cornell.edu>
2023-05-17 00:31:40 -0400

    retain customizations

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# workarond conda-forge/r-base-feedstock#163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+# NB: unsure if this is even used...
+export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
+# This is just to get around a configure failure when trying to link to gdal.
+if [[ ${target_platform} == osx-64 ]]; then
+  export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
+fi
+
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
