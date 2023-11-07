1d0ec34cf97d91c91edd27ad440426d6ad6027d1
Mervin Fansler <mef3005@med.cornell.edu>
2023-11-07 00:29:23 -0500

    restore workarounds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+set -o errexit -o pipefail
 
-export DISABLE_AUTOBREW=1
+# workarond conda-forge/r-base-feedstock#163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+# NB: unsure if this is even used...
+export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
+# This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
+if [[ ${target_platform} == osx-64 ]]; then
+  export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
+fi
 
 # shellcheck disable=SC2086
+export DISABLE_AUTOBREW=1
+
 ${R} CMD INSTALL --build . ${R_ARGS}
